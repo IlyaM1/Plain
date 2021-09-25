4ca339aa2d7fe560c8460aa01c9f14e189be4fae
smasher <57759494+IlyaM1@users.noreply.github.com>
2021-09-25 12:03:53 +0500

    Authorization
    I made authorization with sign up and log in. Users save in MongoDB Atlas.

--- Dialogs/AuthorizationDialog.py
+++ Dialogs/AuthorizationDialog.py
@@ -1,32 +1,41 @@
 from PyQt5 import QtWidgets
 from Widgets.NewRow import NewRow
 from Database import Database
-
+from Dialogs.SignUpDialog import SignUpDialog
 
 class AuthorizationDialog(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
+
+        self.db = Database.get_instance()
         self.layoutAuth = QtWidgets.QVBoxLayout()
 
-        login = NewRow("Login")
-        self.layoutAuth.addLayout(login)
+        self.login = NewRow("Login")
+        self.layoutAuth.addLayout(self.login)
 
-        password = NewRow("Password")
-        self.layoutAuth.addLayout(password)
+        self.password = NewRow("Password")
+        self.layoutAuth.addLayout(self.password)
 
         buttonLog = QtWidgets.QPushButton("Log In")
-        # buttonLog.clicked.connect(self.logInButtonPush)
+        buttonLog.clicked.connect(self.logInButtonPush)
         self.layoutAuth.addWidget(buttonLog)
 
         buttonSign = QtWidgets.QPushButton("Sign Up")
-        # buttonSign.clicked.connect(self.logInButtonPush)
+        buttonSign.clicked.connect(self.signUpButtonPush)
         self.layoutAuth.addWidget(buttonSign)
 
         self.setLayout(self.layoutAuth)
 
-    # def logInButtonPush(self):
-    #     pass
-    #
-    # def signUpButtonPush(self):
-    #     db = Database.get_instance()
-    #     pass
+    def logInButtonPush(self):
+        all_users = self.db.User.User.find()
+        for user in all_users:
+            if self.login.newInput.text() == user['username'] and self.password.newInput.text() == user['password']:
+                print("Entered")
+                print(user)
+            else:
+                print("Wrong Password")
+
+    def signUpButtonPush(self):
+        sign_up = SignUpDialog()
+        sign_up.show()
+        sign_up.exec()
---
+++ Dialogs/SignUpDialog.py
@@ -0,0 +1,38 @@
+from PyQt5 import QtWidgets
+from Widgets.NewRow import NewRow
+from Database import Database
+
+
+class SignUpDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.db = Database.get_instance()
+        self.layoutAuth = QtWidgets.QVBoxLayout()
+
+        self.login = NewRow("Username(will be used as login)")
+        self.layoutAuth.addLayout(self.login)
+
+        self.password = NewRow("Password")
+        self.layoutAuth.addLayout(self.password)
+
+        self.repeat_password = NewRow("Repeat Password")
+        self.layoutAuth.addLayout(self.repeat_password)
+
+
+        buttonSign = QtWidgets.QPushButton("Sign Up")
+        buttonSign.clicked.connect(self.signUpButtonPush)
+        self.layoutAuth.addWidget(buttonSign)
+
+        self.setLayout(self.layoutAuth)
+
+    def signUpButtonPush(self):
+        if self.password.newInput.text() == self.repeat_password.newInput.text():
+            new_user = {
+                'username': self.login.newInput.text(),
+                'password': self.password.newInput.text()
+            }
+            print(new_user)
+            new_user_id = self.db.User.User.insert_one(new_user)
+            print("New User")
+            self.close()
