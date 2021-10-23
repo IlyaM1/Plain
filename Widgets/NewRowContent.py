9c560bba816832737d355e13b0401649e398906f
smasher <57759494+IlyaM1@users.noreply.github.com>
2021-10-23 12:13:19 +0500

    Add files via upload

---
+++ Widgets/NewRowContent.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets
+
+
+class NewRowContent(QtWidgets.QHBoxLayout):
+    def __init__(self, name, content):
+        super().__init__()
+        # self.newLayout = QtWidgets.QHBoxLayout()
+
+        self.newLabelName = QtWidgets.QLabel(name)
+        self.newLabelContent = QtWidgets.QLabel(content)
+
+        self.addWidget(self.newLabelName)
+        self.addWidget(self.newLabelContent)
